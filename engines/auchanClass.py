e14b39540dc3f4b8a4f3b7470bce1704928b6a92
SMAC <196476326+Its-SMAC@users.noreply.github.com>
2026-02-07 16:07:30 +0000

    Update auchanClass.py to be more clean and quite fast

--- engines/auchanClass.py
+++ engines/auchanClass.py
@@ -1,15 +1,24 @@
-from model import Browser, scrapModel
+from playwright.sync_api import sync_playwright
+
+from .model import Browser, scrapModel
 
 
 class Auchan(scrapModel):
     def __init__(self, browser: Browser) -> None:
         super().__init__(browser)
         self.url = "https://www.auchan.pt"
+        self.urlsearch = "/pt/pesquisa?q="
         self.cookieName = "aceitar todos os cookies"
         self.productSelector = ".auc-product"
         self.nameSelector = ".auc-product-tile__name"
         self.priceSelector = ".price .sales .value"
 
-    def search(self, query: str) -> None:
-        self.page.goto(f"{self.url}/pt/pesquisa?q={query}", wait_until="networkidle")
-        self.scrollToBottom()
+
+if __name__ == "__main__":
+    with sync_playwright() as p:
+        browser: Browser = p.chromium.launch(headless=False)
+        auchan: Auchan = Auchan(browser)
+        q: str = input("Qual o produto? ")
+        data: dict[str, str] = auchan.doAll(q)
+        for nome, preco in data.items():
+            print(nome, " | ", preco)
